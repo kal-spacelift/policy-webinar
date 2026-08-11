b6e0acab67d0e7c03ad58b79d9d09d3d2a9078bf
kal <kalyano@spacelift.io>
2026-08-10 23:23:08 -0400

    Change default instance type to t3.micro

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 variable "instance_type" {
   description = "Swap to an off-list type (e.g. m5.4xlarge) to trip the instance-type policy."
   type        = string
-  default     = "t3.medium"
+  default     = "t3.micro"
 }
 
 variable "ssh_ingress_cidr" {
