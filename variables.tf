7e43deda8b403b9c7ef03e9d08ca177e50946d59
kal <kalyano@spacelift.io>
2026-08-11 00:08:52 -0400

    Comment out Environment variable in variables.tf
    Comment out the Environment variable in default map.

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ variable "common_tags" {
   type        = map(string)
   default = {
     Owner              = "platform-team"
-    Environment        = "demo"
+    # Environment        = "demo"
     DataClassification = "public"
   }
 }
